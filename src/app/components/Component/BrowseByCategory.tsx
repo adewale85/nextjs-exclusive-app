e774293ce315dcada10c581bacb23f0752572f4f
Odusola Adewale Iseoluwa <Iseoluwaodu85@gmail.com>
2026-03-19 11:04:04 -0700

    I work on categories items border and images. I added hover, shadow and also make it work on mobile

--- src/app/components/Component/BrowseByCategory.tsx
+++ src/app/components/Component/BrowseByCategory.tsx
@@ -3,17 +3,25 @@ import { browseByCategoryData } from "@/constants/category";
 
 export default function CategoryList() {
   return (
-    <div className="flex gap-4">
+    <div className="grid lg:grid-cols-6 grid-cols-2 gap-4 items-center justify-between py-10 lg:py-20 lg:space-y-0 space-y-6">
       {browseByCategoryData.map((category) => (
-        <div key={category.id} className="border p-12 grid lg:grid-row-6 grid-cols-1 items-center">
-          {/* Render the Image component here using the string from the data */}
-          <Image 
-            src={category.image} 
-            alt={category.title} 
-            width={56} 
-            height={56} 
-          />
-          <p>{category.title}</p>
+        <div 
+          key={category.id} 
+         
+          className="group w-40 h-40 flex flex-col items-center justify-center border border-slate-300 shadow-sm rounded-sm transition-all duration-300
+          hover:bg-red-500 hover:border-red-500 hover:text-white cursor-pointer mx-auto"
+        >
+          <div className="mb-2">
+            <Image 
+              src={category.image} 
+              alt={category.title}
+              width={56} 
+              height={56} 
+              
+              className="object-contain transition-all duration-300 group-hover:brightness-0 group-hover:invert" 
+            />
+          </div>
+          <p className="font-poppins font-medium text-base">{category.title}</p>
         </div>
       ))}
     </div>
--- src/constants/category.ts
+++ src/constants/category.ts
@@ -8,12 +8,12 @@ interface BrowseByCategoryProps {
 }
 
 export const browseByCategoryData: BrowseByCategoryProps[] = [
-  { id: 1, title: "Phones", image: "/images/PhoneImg.svg"  },
-  { id: 2, title: "Laptops", image: "/images/Car.svg" },
-  { id: 3, title: "Headphones", image: "/images/Car.svg"},
-  { id: 4, title: "Watches", image: "/images/Car.svg" },   
-  { id: 5, title: "Watches", image: "/images/Car.svg" },   
-  { id: 6, title: "Watches", image: "/images/Car.svg" },   
+  { id: 1, title: "Phones", image: "/images/CellPhone.svg"  },
+  { id: 2, title: "Laptops", image: "/images/Category-Computer.svg" },
+  { id: 3, title: "Headphones", image: "/images/Category-SmartWatch.svg"},
+  { id: 4, title: "Watches", image: "/images/Category-Camera.svg" },   
+  { id: 5, title: "Watches", image: "/images/Category-Headphone.svg" },   
+  { id: 6, title: "Watches", image: "/images/Category-Gamepad.svg" },   
     
 ];
 
